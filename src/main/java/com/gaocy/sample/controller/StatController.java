6c9094ec11c98d2d25ecba4bd4c08eba0b9677f1
godwin668 <godwin668@gmail.com>
2017-03-24 18:26:22 +0800

    *) update

--- src/main/java/com/gaocy/sample/controller/StatController.java
+++ src/main/java/com/gaocy/sample/controller/StatController.java
@@ -3,6 +3,9 @@ package com.gaocy.sample.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by godwin on 2017/3/19.
  */
@@ -12,7 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class StatController {
 
     @RequestMapping("home")
-    public String loadPage() {
+    public String loadPage(HttpServletRequest req, HttpServletResponse resp) {
+        String remoteAddr = req.getRemoteAddr();
+        String remoteHost = req.getRemoteHost();
+        int remotePort = req.getRemotePort();
+        System.out.println("remoteAddr: " + remoteAddr);
+        System.out.println("remoteHost: " + remoteHost);
+        System.out.println("remotePort: " + remotePort);
         return "index";
     }
 
