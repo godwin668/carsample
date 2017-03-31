4a794868b7f3109a7081c75238d7db3ce62ff503
godwin668 <godwin668@gmail.com>
2017-03-31 14:09:39 +0800

    *) update

--- src/main/java/com/gaocy/sample/util/CityUtil.java
+++ src/main/java/com/gaocy/sample/util/CityUtil.java
@@ -3,14 +3,18 @@ package com.gaocy.sample.util;
 import com.gaocy.sample.spider.SpiderEnum;
 import org.apache.commons.configuration2.Configuration;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by godwin on 2017-03-17.
  */
 public class CityUtil {
+
+    // 中文名称-pinyin
+    private static Map<String, String> cityName2PinyinMap = new HashMap<String, String>();
+
+    // pinyin-中文名称
+    private static Map<String, String> cityPinyin2NameMap = new HashMap<String, String>();
 
     // 城市中文名称-城市英文名称
     private static Map<SpiderEnum, Map<String, String>> spiderCityZH2ENMap = new HashMap<SpiderEnum, Map<String, String>>();
@@ -19,6 +23,7 @@ public class CityUtil {
     private static Map<SpiderEnum, Map<String, String>> spiderCityEN2ZHMap = new HashMap<SpiderEnum, Map<String, String>>();
 
     static {
+        Set<String> cityNameSet = new HashSet<String>();
         for (SpiderEnum spider : SpiderEnum.values()) {
             Map<String, String> cityZH2ENMap = new HashMap<String, String>();
             spiderCityZH2ENMap.put(spider, cityZH2ENMap);
@@ -31,7 +36,14 @@ public class CityUtil {
                 String cityValue = cityConf.getString(cityName);
                 cityZH2ENMap.put(cityName, cityValue);
                 cityEN2ZHMap.put(cityValue, cityName);
+                cityNameSet.add(cityName);
             }
+        }
+
+        for (String cityName : cityNameSet) {
+            String cityPinyin = PinyinUtil.getPinYin(cityName);
+            cityName2PinyinMap.put(cityName, cityPinyin);
+            cityPinyin2NameMap.put(cityPinyin, cityName);
         }
     }
 
@@ -65,11 +77,21 @@ public class CityUtil {
         }
     }
 
+    public static String getNameByPinyin(String pinyin) {
+        return cityPinyin2NameMap.get(pinyin);
+    }
+
+    public static String getPinyinByName(String name) {
+        return cityName2PinyinMap.get(name);
+    }
+
     public static void main(String[] args) {
         System.out.println(SpiderEnum.guazi + ": " + getEName(SpiderEnum.guazi, "北京"));
         System.out.println(SpiderEnum.guazi + ": " + getName(SpiderEnum.guazi, "bj"));
 
         System.out.println("pinyin: " + PinyinUtil.getPinYin("北京"));
 
+        System.out.println("名称-pinyin: 北京_" + getPinyinByName("北京"));
+        System.out.println("pinyin-名称: beijing_" + getNameByPinyin("beijing"));
     }
 }
---
+++ src/main/java/com/gaocy/sample/util/UniqueUtil.java
@@ -0,0 +1,110 @@
+package com.gaocy.sample.util;
+
+import com.alibaba.fastjson.JSON;
+import com.gaocy.sample.spider.SpiderBase;
+import com.gaocy.sample.spider.SpiderEnum;
+import com.gaocy.sample.vo.CarVo;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * Created by godwin on 2017-03-29.
+ */
+public class UniqueUtil {
+
+    protected static DateFormat dfDate = new SimpleDateFormat("yyyyMMdd");
+    protected static File baseDir = new File(ConfUtil.getString("init.log.base.url"));
+
+    public static void main(String[] args) {
+        Set<String> idPriceKeySet = getIdPriceKeySet(SpiderEnum.guazi, 7);
+        System.out.println(JSON.toJSONString(idPriceKeySet));
+        /*
+        Map<String, List<String>> priceDiff = getDiffPrice(SpiderEnum.guazi, 7);
+        for (Map.Entry<String, List<String>> entry : priceDiff.entrySet()) {
+            String srcId = entry.getKey();
+            List<String> priceList = entry.getValue();
+            if (null != priceList && priceList.size() > 1) {
+                SpiderBase.logToFile("diffprice", srcId + " - " + JSON.toJSONString(priceList));
+            } else {
+                SpiderBase.logToFile("uniqueprice", srcId + " - " + JSON.toJSONString(priceList));
+            }
+        }
+        */
+    }
+
+    public static String getKey(String id, String price) {
+        return id + "_" + price;
+    }
+
+    public static Set<String> getIdPriceKeySet(SpiderEnum spider, int days) {
+        Set<String> idPriceKeySet = new HashSet<String>();
+        Map<String, List<String>> diffPriceMap = getDiffPrice(spider, days);
+        if (null != diffPriceMap) {
+            for (Map.Entry<String, List<String>> entry : diffPriceMap.entrySet()) {
+                String id = entry.getKey();
+                List<String> priceList = entry.getValue();
+                idPriceKeySet.add(getKey(id, priceList.get(priceList.size() - 1).substring(9)));
+            }
+        }
+        return idPriceKeySet;
+    }
+
+    /**
+     *
+     * 获取最近N天的样本价格
+     *
+     * @param spider
+     * @param days
+     * @return ID, List(yyyyMMdd price)
+     */
+    public static Map<String, List<String>> getDiffPrice(SpiderEnum spider, int days) {
+        String datePriceRegex = "(\\d{8}) (.*)";
+        Map<String, List<String>> priceListMap = new HashMap<String, List<String>>();
+        for (int i = (-days); i < 0; i++) {
+            File spiderSampleFile = getFile(spider, i);
+            if (!spiderSampleFile.isFile()) {
+                break;
+            }
+            SpiderBase.logToFile("process", spiderSampleFile.getAbsolutePath());
+            try {
+                List<String> sampleLines = FileUtils.readLines(spiderSampleFile, "UTF-8");
+                for (int lineIndex = sampleLines.size() - 1; lineIndex >= 0; lineIndex--) {
+                    String sampleLine = sampleLines.get(lineIndex);
+                    CarVo carVo = JSON.parseObject(sampleLine, CarVo.class);
+                    String srcId = carVo.getSrcId();
+                    String price = carVo.getPrice();
+                    List<String> priceList = priceListMap.get(srcId);
+                    String lastPrice = "";
+                    if (null == priceList) {
+                        priceList = new ArrayList<String>();
+                        priceListMap.put(srcId, priceList);
+                    } else {
+                        String lastDatePriceStr = priceList.get(priceList.size() - 1);
+                        if (StringUtils.isNotBlank(lastDatePriceStr) && lastDatePriceStr.matches(datePriceRegex)) {
+                            lastPrice = lastDatePriceStr.replaceFirst(datePriceRegex, "$2");
+                        }
+                    }
+                    if (null != price && !price.equals(lastPrice)) {
+                        String curDatePriceStr = dfDate.format(DateUtils.addDays(new Date(), i)) + " " + price;
+                        priceList.add(curDatePriceStr);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return priceListMap;
+    }
+
+    public static File getFile(SpiderEnum spider, int addDay) {
+        Date dayBeforeDate = DateUtils.addDays(new Date(), addDay);
+        return new File(baseDir, dfDate.format(dayBeforeDate) + "/" + spider.name().toLowerCase() + ".txt");
+    }
+
+}
