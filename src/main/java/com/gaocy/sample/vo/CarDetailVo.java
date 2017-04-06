385d41834012243e27f07f2606ea73077207f87c
godwin668 <godwin668@gmail.com>
2017-04-06 14:31:34 +0800

    *) update

--- src/main/java/com/gaocy/sample/vo/CarDetailVo.java
+++ src/main/java/com/gaocy/sample/vo/CarDetailVo.java
@@ -2,44 +2,77 @@ package com.gaocy.sample.vo;
 
 import com.gaocy.sample.spider.SpiderEnum;
 
+import java.util.List;
+
 /**
  * Created by godwin on 2017-04-05.
  */
 public class CarDetailVo {
 
-
-    private String id;
-    private String srcId;
-
-    private String name;
-    private String regDate;
-    private String mileage;
-    private String price;
-    private String address;
-
     // 平台来源
-    private SpiderEnum src;
+    private String src;
 
     // 城市
     private String city;
 
-    // 品牌
-    // 车系
-    // 车型（名称和ID）
-    // 车辆颜色
-    // 行驶里程
-    // 价格
-    // 上牌时间
-    // 联系人
-    // 联系电话
-    // 看车地址
-    // 图片
-    // 身份类型
-    // 商家名称
-    // 商家id
-    // 帖子状态
+    // 帖子ID
+    private String id;
+
     // 原帖链接
+    private String url;
+
+    // 名称
+    private String name;
+
+    // 品牌
+    private String brand;
+
+    // 车系
+    private String series;
+
+    // 车型（名称和ID）
+    private String model;
+
+    // 车辆颜色
+    private String color;
+
+    // 行驶里程
+    private String mileage;
+
+    // 价格
+    private String price;
+
     // 发布时间
+    private String postDate;
+
+    // 上牌时间
+    private String regDate;
+
+    // 联系人
+    private String contact;
+
+    // 联系电话
+    private String phone;
+
+    // 看车地址
+    private String address;
+
+    // 图片
+    private List<String> images;
+
+    // 身份类型
+    private String identity;
+
+    // 商家名称
+    private String bizName;
+
+    // 商家id
+    private String bizId;
+
+    // 帖子状态
+    private String status;
+
     // 帖子标签
+    private String tag;
 
 }
