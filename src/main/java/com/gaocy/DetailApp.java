379033007b2c57b054d19e10e7cacaea277cf3d5
Godwin <godwin668@gmail.com>
2017-04-23 01:31:02 +0800

    *) update

--- src/main/java/com/gaocy/DetailApp.java
+++ src/main/java/com/gaocy/DetailApp.java
@@ -6,12 +6,16 @@ import com.gaocy.sample.spider.SpiderBase;
 import com.gaocy.sample.spider.SpiderEnum;
 import com.gaocy.sample.spider.SpiderFactory;
 import com.gaocy.sample.util.CityUtil;
+import com.gaocy.sample.util.ConfUtil;
 import com.gaocy.sample.vo.CarDetailVo;
 import com.gaocy.sample.vo.CarVo;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.Date;
-import java.util.List;
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,9 +23,13 @@ import java.util.concurrent.Executors;
 /**
  * Created by godwin on 2017-03-16.
  */
-public class DetailApp extends DetailBaseApp implements Callable {
+public class DetailApp implements Callable {
 
     public static final ExecutorService es = Executors.newFixedThreadPool(10);
+
+    protected static DateFormat dfDate = new SimpleDateFormat("yyyyMMdd");
+    protected static DateFormat dfDateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    protected static File baseDir = new File(ConfUtil.getString("init.log.base.url"));
 
     private String dateStr;
     private SpiderEnum spiderEnum;
@@ -41,6 +49,44 @@ public class DetailApp extends DetailBaseApp implements Callable {
             DetailApp app = new DetailApp(dateStr, spider, cityArr);
             es.submit(app);
         }
+    }
+
+    /**
+     * 获取列表页车源信息
+     *
+     * @param spider 抓取源
+     * @param city 城市
+     * @param dateStr 日期
+     * @return
+     */
+    public static List<CarVo> listCarVo(SpiderEnum spider, String city, String dateStr) {
+        List<CarVo> carVoList = new ArrayList<CarVo>();
+        File todayFile = new File(baseDir, dateStr + "/" + spider.name().toLowerCase() + ".txt");
+        if (todayFile.isFile()) {
+            try {
+                List<String> sampleLines = FileUtils.readLines(todayFile, "UTF-8");
+                for (int lineIndex = sampleLines.size() - 1; lineIndex >= 0; lineIndex--) {
+                    String sampleLine = sampleLines.get(lineIndex);
+                    CarVo carVo = JSON.parseObject(sampleLine, CarVo.class);
+                    String srcId = carVo.getSrcId();
+                    String carCity = carVo.getCity();
+                    if (null != city && city.equals(carCity)) {
+                        carVoList.add(carVo);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        Collections.sort(carVoList, new Comparator<CarVo>() {
+            @Override
+            public int compare(CarVo o1, CarVo o2) {
+                String id1 = o1.getSrcId();
+                String id2 = o2.getSrcId();
+                return id1.compareTo(id2);  // 递增
+            }
+        });
+        return carVoList;
     }
 
     @Override
